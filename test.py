59d512fa1b1efae7b5bfddcc7208a3706757e5b7
Jenna Atkinson <>
2022-10-18 20:21:08 -0600

    Add function names

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 #Tests for computeShortestPaths()
 
 # Test single node as start node and end node
-#def should_returnZero_when_singleNode():
-
+def should_returnZero_when_singleNode():
+  return
 
 # Test two nodes, one as start and one as end
+def should_returnEdgeLen_when_twoNodesOneEdge():
+  return
 
 # Test two nodes with no path between them
+def should_returnInf_when_noPath():
+  return
 
 # Test three nodes, with the single edge to dest being larger, but the two combined edges being smaller
+def should_returnSmallerCombinedEdge_when_multipleNodesMultipleEdges():
+  return
 
 # Test example from slides
+def should_pass_when_complexGraph():
+  return
