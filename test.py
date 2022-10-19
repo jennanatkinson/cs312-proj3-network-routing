b8097a4af3a775bb5745e86a9517d46cc87c3c6c
Jenna Atkinson <>
2022-10-18 20:19:04 -0600

    Add test file

---
+++ test.py
@@ -0,0 +1,13 @@
+#Tests for computeShortestPaths()
+
+# Test single node as start node and end node
+#def should_returnZero_when_singleNode():
+
+
+# Test two nodes, one as start and one as end
+
+# Test two nodes with no path between them
+
+# Test three nodes, with the single edge to dest being larger, but the two combined edges being smaller
+
+# Test example from slides
